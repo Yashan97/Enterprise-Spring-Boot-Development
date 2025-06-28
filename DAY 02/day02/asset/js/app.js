430fde66018b30154ed901e27abe5803b37f2051
Yashan97 <s.d.y.anupma@gmail.com>
2025-06-28 22:24:42 +0530

    connecting backend with database

--- DAY 02/day02/asset/js/app.js
+++ DAY 02/day02/asset/js/app.js
@@ -1,12 +1,40 @@
-function loadProducts(){
-    fetch("http://localhost:8080/get-new%20products")
-    .then(res =>res.json())
-    .then(res => {
-        console.log(res);
-        
-    })
-    
-}
 
-document.getElementById("btn-load-product")
-    .addEventListener("click", loadProducts ) 
+let txtProductName
+    = document.getElementById("txt-product");
+
+let txtProductDescription 
+    = document.getElementById("txt-product-description");
+
+let txtProductPrice 
+    = document.getElementById("txt-product-price");
+
+
+    let btnSaveProduct 
+    = document.getElementById("btn-save-product");
+
+function btnSaveProductClick(){ // create method for button
+        //Gather text box values
+        let requestBody = {
+            // Create Json object and pased value
+            "name":txtProductName.value,
+            "description":txtProductDescription.value,
+            "price":txtProductPrice.value
+
+        };
+        //POTS
+        //  Connecting network request and json Object (intergating frontend and backend)
+        fetch("http://localhost:8080/create-product",{
+            method:"POST",
+            body:JSON.stringify(requestBody),
+            headers:{
+                "Content-type":"application/json",
+            }
+        })
+        .then(data =>data.json())
+        .then(data=>{
+            console.log("response receved");
+            
+        })
+    }
+    // call button 
+    btnSaveProduct.addEventListener("click",btnSaveProductClick);
--- DAY 02/product-service/src/main/java/edu/icet/ecom/controller/ProductController.java
+++ DAY 02/product-service/src/main/java/edu/icet/ecom/controller/ProductController.java
@@ -1,19 +1,16 @@
 package edu.icet.ecom.controller;
 
 import edu.icet.ecom.module.Product;
-import edu.icet.ecom.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class ProductController {
-    @GetMapping("/get-new products")
-   ArrayList<Product> getProduct(){
-        ProductService productService = new ProductService();
-        ArrayList<Product> productList = productService.getProductList();
-        System.out.println(productList);
-        return productList;
+    @PostMapping("/create-product")
+    void createProduct(@RequestBody Product product){
+        System.out.println(product);
+
+
+
     }
 }
